980e057c2925137eb46d96adc7b0859b6b1e2018
ariamottaghi-bit <amottaghi@ucsd.edu>
2024-10-16 18:59:27 -0700

    y

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from digitalio import DigitalInOut, Direction
 from analogio import AnalogIn
 from time import sleep
 
+
+
 # setup pins
 microphone = AnalogIn(board.IO1)
 
@@ -11,10 +13,18 @@ status.direction = Direction.OUTPUT
 
 led_pins = [
     board.IO21,
-    board.IO26, # type: ignore
+    board.IO26, #type: ignore,
     board.IO47,
-    # do the rest...
+    board.IO33, #type: ignore,
+    board.IO34, #type: ignore,
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
 ]
+
 
 leds = [DigitalInOut(pin) for pin in led_pins]
 
@@ -26,12 +36,17 @@ while True:
     volume = microphone.value
 
     print(volume)
+    
+    for led in leds:
+        led.value = 0
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    for i, led in enumerate(leds):
+        if volume > (20000 + i * 2000):
+            led.value = 1
 
     sleep(1)
 
     # instead of blinking,
     # how can you make the LEDs
     # turn on like a volume meter?
+
